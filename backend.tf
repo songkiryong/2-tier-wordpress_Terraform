ef711c55de30b35170ff9cd357fff22d1287bf92
songkiryong <73922068+songkiryong@users.noreply.github.com>
2021-09-13 02:04:37 +0900

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-    backend "s3" { 
-      bucket         = "song-tfstate" 
-      key            = "terraform/terraform.tfstate" 
-      region         = "ap-northeast-2"  
-      encrypt        = true
-      dynamodb_table = "terraform-lock" 
-    }
-}
